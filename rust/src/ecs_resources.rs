e2ff1ffeeba51f4167ecb2e17157a7ae67a220a0
indubitablement2 <jeanmichel88@hotmail.com>
2021-10-07 19:50:23 -0400

    clean up

--- rust/src/ecs_resources.rs
+++ rust/src/ecs_resources.rs
@@ -3,11 +3,36 @@ use gdnative::core_types::TypedArray;
 
 /// Modify the game.
 pub struct GameParameterRes {
-    pub drag: f32, // Velocity is multiplied by this each tick.
+    /// Velocity is multiplied by this each tick.
+    pub drag: f32,
+    /// How many seconds a day last.
+    pub day_lenght: f64,
+}
+impl Default for GameParameterRes {
+    fn default() -> Self {
+        Self {
+            drag: 0.75,
+            day_lenght: 86400.0,
+        }
+    }
 }
 
 pub struct TimeRes {
-    pub tick: u32,
+    /// Number of days elapsed.
+    pub days: u32,
+    /// Time in seconds for this day.
+    pub time: f64,
+    /// Time elapsed since last update.
+    pub delta: f64,
+}
+impl Default for TimeRes {
+    fn default() -> Self {
+        Self {
+            days: 0,
+            time: 0.0,
+            delta: 0.0,
+        }
+    }
 }
 
 // ! Physic
@@ -16,6 +41,16 @@ pub struct TimeRes {
 
 /// All that is needed to render sprites.
 pub struct RenderRes {
-    pub render_data: Option<TypedArray<f32>>,
+    /// This is the bulk array that is needed for the multimesh.
+    pub render_data: TypedArray<f32>,
+    /// This is used to cull extra uneeded sprite from render_data.
     pub visible_instance: i64,
+}
+impl Default for RenderRes {
+    fn default() -> Self {
+        Self {
+            render_data: Default::default(),
+            visible_instance: Default::default(),
+        }
+    }
 }
