5e367ca8f78fe44932e2adcfcb7471a3213ad9cb
indubitablement2 <jeanmichel88@hotmail.com>
2021-11-25 17:26:04 -0500

    moved to standalone server

--- common/src/res_parameters.rs
+++
@@ -1,7 +0,0 @@
-/// Global Metascape parameters.
-pub struct ParametersRes {
-    /// The maximum distance to the center.
-    pub bound: f32,
-    /// Multiply fleet velocity every tick.
-    pub movement_friction: f32,
-}
--- common/src/res_times.rs
+++
@@ -1,8 +0,0 @@
-pub struct TimeRes {
-    pub tick: u32,
-}
-impl TimeRes {
-    pub fn new() -> Self {
-        Self { tick: 0 }
-    }
-}
