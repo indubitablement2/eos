39619738174737652db96de2cef0b158b250bfcf
indubitablement2 <62550921+indubitablement2@users.noreply.github.com>
2022-10-18 10:28:53 -0400

    add battlescape replay container

--- battlescape/src/commands.rs
+++ battlescape/src/commands.rs
@@ -32,3 +32,50 @@ pub enum BattlescapeCommand {
     SetPlayerInput(SetPlayerInput),
     // PlayerControlShip(PlayerControlShip),
 }
+
+#[derive(Clone, Serialize, Deserialize, Default)]
+pub struct Replay {
+    #[serde(skip)]
+    remaining: Vec<(u64, Vec<BattlescapeCommand>)>,
+    pub cmds: Vec<Vec<BattlescapeCommand>>,
+}
+impl Replay {
+    pub fn push_cmds(&mut self, tick: u64, cmds: Vec<BattlescapeCommand>) {
+        let mut next_tick = self.cmds.len() as u64;
+
+        if tick == next_tick {
+            self.cmds.push(cmds);
+
+            // Maybe we can use some of the remaining ticks now.
+            next_tick = self.cmds.len() as u64;
+            loop {
+                if let Some((tick, _)) = self.remaining.first() {
+                    if *tick == next_tick {
+                        self.cmds.push(self.remaining.remove(0).1);
+                    } else {
+                        // Can not use that tick yet.
+                        break;
+                    }
+                } else {
+                    // No more remaining tick.
+                    break;
+                }
+            }
+        } else if tick < next_tick {
+            // Already have that tick.
+            log::debug!("Replay received tick {} twice while waiting for tick {}. Ignoring...", tick, next_tick);
+        } else {
+            match self.remaining.binary_search_by(|probe| {
+                probe.0.cmp(&tick)
+            }) {
+                Ok(_) => {
+                    // Already have that tick.
+                    log::debug!("Replay received tick {} twice while waiting for tick {}. Ignoring...", tick, next_tick);
+                }
+                Err(i) =>  {
+                    self.remaining.insert(i, (tick, cmds));
+                }
+            }
+        }
+    }
+}
