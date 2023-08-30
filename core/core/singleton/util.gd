da6b0c7e768b7d57ba22f3de0a084902883335b5
Jean-Michel Nadeau <jeanmichel88@hotmail.com>
2023-08-30 14:25:18 -0400

    add position prediction functions

--- core/core/singleton/util.gd
+++ core/core/singleton/util.gd
@@ -15,6 +15,7 @@ static func angle_up(angle: float) -> float:
 
 ## target in "local space" (target - my_position).
 ## Return position in the same local space.
+## ~4 iterations will give good result.
 static func predict_position(
 	target: Vector2,
 	target_velocity: Vector2,
@@ -22,4 +23,20 @@ static func predict_position(
 	iteration: int) -> Vector2:
 	for i in iteration:
 		var time_to_target := target.length() / speed
+		target += target_velocity * time_to_target 
 	return target
+
+
+## See `predict_position`
+## This use global space instead.
+static func predict_position_global(
+	from: Vector2,
+	global_target: Vector2,
+	target_velocity: Vector2,
+	speed: float,
+	iteration: int) -> Vector2:
+	var target = global_target - from
+	for i in iteration:
+		var time_to_target := target.length() / speed
+		target += target_velocity * time_to_target 
+	return target + from
