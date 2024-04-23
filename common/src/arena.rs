082f7b1515ce822f88720a95a69459a9bbc6602e
Jean-Michel Nadeau <jeanmichel88@hotmail.com>
2024-04-23 18:22:56 -0400

    Finish generational arena

--- common/src/arena.rs
+++ common/src/arena.rs
@@ -8,6 +8,8 @@ enum Bucket<V> {
     Occupied { value: V },
 }
 
+/// Generational arena which prioritizes reusing index
+/// that would serialize in the least bytes.
 pub struct Arena<K, V> {
     /// < 128, < 16384, Rest
     heads: [Option<u32>; 3],
@@ -21,20 +23,30 @@ impl<K: Into<NonZeroU64> + From<NonZeroU64> + Copy + Sized, V> Arena<K, V> {
         K::from(NonZeroU64::new((index as u64) << 32 | generation.get() as u64).unwrap())
     }
 
-    fn generation(key: K) -> NonZeroU32 {
+    fn key_generation(key: K) -> NonZeroU32 {
         let u: NonZeroU64 = key.into();
         NonZeroU32::new(u.get() as u32).unwrap()
     }
 
-    fn index(key: K) -> usize {
+    fn key_index(key: K) -> usize {
         let u: NonZeroU64 = key.into();
         (u.get() >> 32) as usize
+    }
+
+    fn find_step(index: u32) -> usize {
+        if index < Self::STEPS[0] {
+            return 0;
+        } else if index < Self::STEPS[1] {
+            return 1;
+        } else {
+            return 2;
+        }
     }
 
     pub fn get_index(&self, index: usize) -> Option<&V> {
         let (_, bucket) = self.data.get(index)?;
         match bucket {
-            Bucket::Empty { next_empty: _ } => None,
+            Bucket::Empty { .. } => None,
             Bucket::Occupied { value } => Some(value),
         }
     }
@@ -42,63 +54,60 @@ impl<K: Into<NonZeroU64> + From<NonZeroU64> + Copy + Sized, V> Arena<K, V> {
     pub fn get_mut_index(&mut self, index: usize) -> Option<&mut V> {
         let (_, bucket) = self.data.get_mut(index)?;
         match bucket {
-            Bucket::Empty { next_empty: _ } => None,
+            Bucket::Empty { .. } => None,
             Bucket::Occupied { value } => Some(value),
         }
     }
 
-    pub fn remove_index(&mut self, index: usize) -> Option<V> {
-        // TODO:
-        None
-    }
-
     pub fn get(&self, key: K) -> Option<&V> {
-        let (generation, bucket) = self.data.get(Self::index(key))?;
+        let (generation, bucket) = self.data.get(Self::key_index(key))?;
 
-        if Self::generation(key) != *generation {
+        if Self::key_generation(key) != *generation {
             return None;
         }
 
         match bucket {
-            Bucket::Empty { next_empty } => None,
+            Bucket::Empty { .. } => None,
             Bucket::Occupied { value } => Some(value),
         }
     }
 
     pub fn get_mut(&mut self, key: K) -> Option<&mut V> {
-        let (generation, bucket) = self.data.get_mut(Self::index(key))?;
+        let (generation, bucket) = self.data.get_mut(Self::key_index(key))?;
 
-        if Self::generation(key) != *generation {
+        if Self::key_generation(key) != *generation {
             return None;
         }
 
         match bucket {
-            Bucket::Empty { next_empty: _ } => None,
+            Bucket::Empty { .. } => None,
             Bucket::Occupied { value } => Some(value),
         }
     }
 
-    pub fn remove(&mut self, key: K) -> Option<V> {
-        let (generation, bucket) = self.data.get_mut(Self::index(key))?;
-        if Self::generation(key) != *generation {
-            return None;
+    pub fn remove(&mut self, key: K) -> bool {
+        let index = Self::key_index(key);
+        let Some((generation, bucket)) = self.data.get_mut(index) else {
+            return false;
+        };
+
+        if Self::key_generation(key) != *generation {
+            return false;
         }
 
-        // TODO
-        todo!()
-        // match bucket {
-        //     Bucket::Empty { next_empty: _ } => None,
-        //     Bucket::Occupied { value } => {
-        //         let new_generation = generation.checked_add(1).unwrap_or(NonZeroU32::MIN);
-        //         *generation = new_generation;
+        match bucket {
+            Bucket::Empty { .. } => false,
+            Bucket::Occupied { .. } => {
+                let step = Self::find_step(index as u32);
 
-        //         *bucket = Bucket::Empty {
-        //             next_empty: self.heads[0],
-        //         };
-        //         self.heads[0] = Some(Self::index(key) as u32);
-        //         Some(*value)
-        //     }
-        // }
+                *bucket = Bucket::Empty {
+                    next_empty: self.heads[step],
+                };
+                self.heads[step] = Some(index as u32);
+
+                true
+            }
+        }
     }
 
     pub fn insert(&mut self, value: V) -> K {
@@ -109,7 +118,7 @@ impl<K: Into<NonZeroU64> + From<NonZeroU64> + Copy + Sized, V> Arena<K, V> {
                     Bucket::Empty { next_empty } => {
                         *head = *next_empty;
                     }
-                    Bucket::Occupied { value: _ } => unreachable!(),
+                    Bucket::Occupied { .. } => unreachable!(),
                 }
                 *bucket = Bucket::Occupied { value };
                 return K::from(
@@ -119,12 +128,8 @@ impl<K: Into<NonZeroU64> + From<NonZeroU64> + Copy + Sized, V> Arena<K, V> {
         }
 
         let index = self.data.len() as u32;
-        for (i, step) in Self::STEPS.iter().enumerate() {
-            if index <= *step {
-                self.heads[i] = Some(index);
-                break;
-            }
-        }
+        self.heads[Self::find_step(index)] = Some(index);
+
         let key = K::from(NonZeroU64::new((index as u64) << 32 | 1).unwrap());
         self.data
             .push((NonZeroU32::MIN, Bucket::Occupied { value }));
@@ -135,18 +140,25 @@ impl<K: Into<NonZeroU64> + From<NonZeroU64> + Copy + Sized, V> Arena<K, V> {
     pub fn leak_take_index(&mut self, index: usize) -> (K, V) {
         let (generation, bucket) = &mut self.data[index];
         match std::mem::replace(bucket, Bucket::Empty { next_empty: None }) {
-            Bucket::Empty { next_empty: _ } => panic!(),
+            Bucket::Empty { .. } => panic!(),
             Bucket::Occupied { value } => (Self::key_new(index as u32, *generation), value),
         }
     }
 
     pub fn unleak_remove_index(&mut self, index: usize) {
-        let (_, bucket) = self.data.get_mut(index).unwrap();
-        // TODO
+        let (generation, bucket) = &mut self.data[index];
+        *generation = generation.checked_add(1).unwrap_or(NonZeroU32::MIN);
+
+        let step = Self::find_step(index as u32);
+
+        *bucket = Bucket::Empty {
+            next_empty: self.heads[step],
+        };
+        self.heads[step] = Some(index as u32);
     }
 
     pub fn unleak_set_index(&mut self, index: usize, value: V) {
-        let (_, bucket) = self.data.get_mut(index).unwrap();
+        let (_, bucket) = &mut self.data[index];
         *bucket = Bucket::Occupied { value };
     }
 }
